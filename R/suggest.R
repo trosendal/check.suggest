19cfb97da16f237b0e603c1cd693f2a7f6352e33
Thomas Rosendal <thomas.rosendal@sva.se>
2023-01-23 14:41:16 +0100

    use with string or path

--- R/suggest.R
+++ R/suggest.R
@@ -3,18 +3,32 @@
 ##' A function that reads the output from R CMD check and if it finds
 ##' some common patterns it suggests solutions.
 ##'
+##' @param log A string containing the check log
 ##' @param path The path to the check log file
 ##' @importFrom rjson fromJSON
 ##' @export
 ##' @return invisible(NULL)
-suggest <- function(path) {
+suggest <- function(log, path) {
     x <- system.file("extdata/patterns.json",
                      package = "check.suggest")
     x <- rjson::fromJSON(file = x)
-    y <- readLines(path)
+
+    ml <- missing(log)
+    mp <- missing(path)
+
+    if (ml) {
+        if (mp) {
+            stop("One of log or path must be supplied to suggest")
+        }
+        log <- readLines(path)
+    }
+    if (!ml && !mp) {
+        stop("Only one of log or path must be supplied to suggest")
+    }
+
     for (i in seq_along(x)) {
         message <- NULL
-        if (any(grepl(x[[i]]["pattern"], y)))
+        if (any(grepl(x[[i]]["pattern"], log)))
             message <- paste0(strwrap(x[[i]]["suggestion"]), "\n")
         cat(message, "\n")
     }
--- tests/suggest.R
+++ tests/suggest.R
@@ -1,10 +1,20 @@
 library("check.suggest")
 
 path <- system.file("extdata/examples/0001check.log", package = "check.suggest")
-suggest(path)
+suggest(path = path)
 
 path <- system.file("extdata/examples/0002check.log", package = "check.suggest")
-suggest(path)
+suggest(path = path)
 
 path <- system.file("extdata/examples/0003check.log", package = "check.suggest")
-suggest(path)
+suggest(path = path)
+
+ob <- tools::assertError(suggest(log = "", path = path))[[1]]$message
+ex <- paste("Only one of log or path must be supplied to suggest")
+stopifnot(length(grep(ex, ob)) == 1L)
+
+ob <- tools::assertError(suggest())[[1]]$message
+ex <- paste("One of log or path must be supplied to suggest")
+stopifnot(length(grep(ex, ob)) == 1L)
+
+suggest(log = "")
